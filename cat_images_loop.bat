d8e014427fed02fba18b58f8c44faea47afad011
arandomperson <146173916+arandomperson137@users.noreply.github.com>
2025-02-07 09:43:36 +0000

    Update cat_images_loop.bat
    :p

--- cat_images_loop.bat
+++ cat_images_loop.bat
@@ -1,17 +1,12 @@
 @echo off
-:: Loop infinito para abrir imagens de gatos aleatórias e abre a script continuamente.
 
-:: Define a URL da API de imagens de gatos aleatórios
 set "cat_api=https://cataas.com/cat"
 
 :loop
-:: Usa o comando start para abrir a imagem aleatória de gato
 start "" "%cat_api%"
 
-:: Reabre a script continuamente
 start "" "%~f0"
 
-:: Aguarda para evitar loop infinito muito rápido
 timeout /t 1 >nul
 
 goto loop
